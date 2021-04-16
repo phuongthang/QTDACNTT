40d8d97d3921a1b1c4cb5da09a15663a0a3ff670
Phương Thắng <thang.pc@beetechsoft.vn>
2021-04-16 22:34:39 +0700

    create application base

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# QTDACNTT
+# DevC
+
+Download model at: 
+https://drive.google.com/file/d/1Z-6ET2qCEuVJ9jze3L3l2ahdo-Z2CYz1/view then reaname to "logistic_model.pkl" and add to app/resources/trained_models
+
+https://drive.google.com/file/d/1LC-BGMIw3dShOUmwHJbMTqf7aif6F1KB/view then reaname to "svm_model.pkl" and add to app/resources/trained_models
+
+Install libraries in requirements.txt
+
+Run run.py to start server
---
+++ app/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+
+from app.api.campaign_api import campaign
+from app.api.comment_api import comment
+from app.api.predict_api import predict
+from app.api.post_api import post
+from app.database import db
+from app.services import main_service
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"db": "devc", "host": "localhost", "port": 27017}
+app.register_blueprint(predict)
+app.register_blueprint(campaign)
+app.register_blueprint(comment)
+app.register_blueprint(post)
+
+db.initialize_db(app)
+model = main_service.init_model()
+
+
+@app.route("/")
+def index():
+    return render_template("authen/login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/show")
+def show():
+    return render_template("campaign/index.html")
+
+@app.route("/create")
+def create():
+    return render_template("campaign/create.html")
+
+
+@app.route("/detail")
+def detail():
+    return render_template("campaign/detail.html")
+
+@app.route("/comments")
+def comments():
+    return render_template("campaign/comment.html")
+
+@app.route("/predicts")
+def predicts():
+    return render_template("campaign/predicts.html")
+
+app.run()
