38b58821e50a90659ea2e2c3d858b68c0a4f0545
Long Nguyen <nhatlong6122@gmail.com>
2021-04-25 15:42:35 +0700

    add facebook login

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, Blueprint
 from app.api.campaign_api import campaign
 from app.api.comment_api import comment
 from app.api.predict_api import predict
 from app.api.post_api import post
 from app.database import db
 from app.services import main_service
+from app.services.facebook import fb
+
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {"db": "devc", "host": "localhost", "port": 27017}
@@ -13,6 +14,7 @@ app.register_blueprint(predict)
 app.register_blueprint(campaign)
 app.register_blueprint(comment)
 app.register_blueprint(post)
+app.register_blueprint(fb)
 
 db.initialize_db(app)
 model = main_service.init_model()
@@ -48,5 +50,6 @@ def comments():
 @app.route("/predicts")
 def predicts():
     return render_template("campaign/predicts.html")
+
 
 app.run()
